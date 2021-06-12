43f9a688df6ae572b8e57607e2550d9bfe262b53
e444er <e444er@@gmail.com>
2021-06-12 13:12:55 +0600

    homework 1

--- KotlinBasic/01_BasicSyntax/app/src/main/java/com/devv/basic/go.kt
+++ KotlinBasic/01_BasicSyntax/app/src/main/java/com/devv/basic/go.kt
@@ -1,19 +1,17 @@
 package com.devv.basic
 
 fun main() {
-    val firstName: String = "Бауыржан"
-    val lastName: String = "Тастанбеков"
+    val firstName = "Бауыржан"
+    val lastName = "Тастанбеков"
     var height: Int = 170
     val weight: Float = 70.0f
     var isChild: Boolean = height > 150 && weight > 40
-    var info: String = "Имя = ${firstName}, Фамилия = ${lastName}, " +
-            "Рост = ${height}, Вес = ${weight}, Ребенок = ${isChild}"
+    var info = "Имя = $firstName, Фамилия = $lastName, " +
+            "Рост = $height, Вес = $weight, Ребенок = $isChild"
     println(info)
-            height = 50
+    height = 50
     isChild = height > 150 && weight > 40
-     info = "Имя = ${firstName}, Фамилия = ${lastName}, " +
-            "Рост = ${height}, Вес = ${weight}, Ребенок = ${isChild}"
+    info = "Имя = $firstName, Фамилия = $lastName, " +
+            "Рост = $height, Вес = $weight, Ребенок = $isChild"
     println(info)
-
-
 }
