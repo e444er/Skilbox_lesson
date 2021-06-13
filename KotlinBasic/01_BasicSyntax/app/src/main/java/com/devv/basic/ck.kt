cedf600fc62fdfb542fd1bc44142282292ae5a14
e444er <e444er@@gmail.com>
2021-06-13 13:36:01 +0600

    homework 2

--- KotlinBasic/01_BasicSyntax/app/src/main/java/com/devv/basic/ck.kt
+++
@@ -1,7 +0,0 @@
-package com.devv.basic
-
-fun main() {
-    println("число")
-    val n:Int = readLine()?.toIntOrNull() ?: return
-    println("число $n")
-}
--- KotlinBasic/01_BasicSyntax/app/src/main/java/com/devv/basic/go.kt
+++ KotlinBasic/01_BasicSyntax/app/src/main/java/com/devv/basic/go.kt
@@ -3,9 +3,9 @@ package com.devv.basic
 fun main() {
     val firstName = "Бауыржан"
     val lastName = "Тастанбеков"
-    var height: Int = 170
-    val weight: Float = 70.0f
-    var isChild: Boolean = height > 150 && weight > 40
+    var height = 170
+    val weight = 70.0f
+    var isChild = height > 150 && weight > 40
     var info = "Имя = $firstName, Фамилия = $lastName, " +
             "Рост = $height, Вес = $weight, Ребенок = $isChild"
     println(info)
