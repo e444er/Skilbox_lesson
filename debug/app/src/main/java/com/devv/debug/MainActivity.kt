4f78e66716440a07eb7ce5c98320d14c3c239f76
e444er <e444er@@gmail.com>
2021-06-15 21:00:02 +0600

    home release

---
+++ debug/app/src/main/java/com/devv/debug/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.devv.debug
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.text)
+        textView.text = """
+            Build type = ${BuildConfig. BUILD_TYPE}
+            Flavor = ${BuildConfig.FLAVOR}
+            VersionCode = ${BuildConfig.VERSION_CODE}
+            VersionName = ${BuildConfig.VERSION_NAME}
+            ApplicationId = ${BuildConfig.APPLICATION_ID}
+        """
+
+    }
+}
