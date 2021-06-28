d02c5dc516f412a024076dc971fa2afd03bb8404
e444er <e444er@@gmail.com>
2021-06-28 17:56:49 +0600

    printDescription

--- ClassesAndInheritanc/app/src/main/java/com/devv/classesandinheritanc/ClassesAndInheritance.kt
+++ ClassesAndInheritanc/app/src/main/java/com/devv/classesandinheritanc/ClassesAndInheritance.kt
@@ -2,72 +2,72 @@ package com.devv.classesandinheritanc
 
 fun main() {
     val a = Room(area = 6.2)
-    a.getDescription()
+    a.printDescription()
 
     val bedRoom = BedRoom()
-    bedRoom.getDescription()
+    bedRoom.printDescription()
 
     val livingRoom = LivingRoom()
-    livingRoom.getDescription()
+    livingRoom.printDescription()
 
     val kitchen = Kitchen()
-    kitchen.getDescription()
+    kitchen.printDescription()
 
     val bathRoom = BathRoom()
-    bathRoom.getDescription()
+    bathRoom.printDescription()
 
     val childrenRoom = ChildrenRoom("детская комната")
-    childrenRoom.getDescription()
+    childrenRoom.printDescription()
 
     val roomIsFree = RoomIsFree("комната свободная")
-    roomIsFree.getDescription()
+    roomIsFree.printDescription()
 }
 
 open class Room constructor(val area: Double) {
 
     protected open val title: String = "Обычная комната"
-    open fun getDescription() {
+    open fun printDescription() {
         println("Название комнаты: $title, Площадь комнаты: $area м. кв.")
     }
 }
 
 class BedRoom() : Room(area = 4.6) {
     override val title: String = "спальня"
-    override fun getDescription() {
+    override fun printDescription() {
         println("Название комнаты: $title, Площадь комнаты: $area м. кв.")
     }
 }
 
 class LivingRoom() : Room(area = 8.6) {
     override val title: String = "гостевая"
-    override fun getDescription() {
+    override fun printDescription() {
         println("Название комнаты: $title, Площадь комнаты: $area м. кв.")
     }
 }
 
 class Kitchen() : Room(area = 9.5) {
     override val title: String = "кухня"
-    override fun getDescription() {
+    override fun printDescription() {
         println("Название комнаты: $title, Площадь комнаты: $area м. кв.")
     }
 }
 
 class BathRoom() : Room(area = 3.5) {
     override val title: String = "санузел"
-    override fun getDescription() {
+    override fun printDescription() {
         println("Название комнаты: $title, Площадь комнаты: $area м. кв.")
     }
 }
 
 
 class ChildrenRoom(val titleChildrenRoom: String) : Room(area = 5.5) {
-    override fun getDescription() {
+    override fun printDescription() {
         println("Название комнаты: $titleChildrenRoom, Площадь комнаты: $area м. кв.")
     }
 }
 
 class RoomIsFree(val titleRoomIsFree: String) : Room(area = 7.0) {
-    override fun getDescription() {
+    override fun printDescription() {
         println("Название комнаты: $titleRoomIsFree, Площадь комнаты: $area м. кв.")
     }
 }
