58f8d604d0f85d3e5f8422127bc6b0676b1a22bf
Бауыржан Тастанбеков <e444er@gmail.com>
2021-07-01 10:26:55 +0000

    Update Main.kt

--- KotlinBasic/04_Collections/app/src/main/java/com/devv/BaseMethods/Main.kt
+++ KotlinBasic/04_Collections/app/src/main/java/com/devv/BaseMethods/Main.kt
@@ -1,11 +1,11 @@
 package com.devv.BaseMethods
 
 fun main() {
-    val person1 = Person(energy = 173, weight = 67, name = "Eminem")
-    val person2 = Person(energy = 170, weight = 67, name = "Бауыржан")
+    val person1 = Person(height = 173, weight = 67, name = "Eminem")
+    val person2 = Person(height = 170, weight = 67, name = "Бауыржан")
     val people = mutableSetOf(person1, person2)
     println(people.count())
-    val person3 = Person(energy = 180, weight = 90, name = "Бэтман")
+    val person3 = Person(height = 180, weight = 90, name = "Бэтман")
     people.add(person3)
     people.forEach() {
         println(it.buyPet())
